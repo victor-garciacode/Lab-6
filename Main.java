89fe76e8600ec62d774f6512517c9b166f67d8bf
garciavd <89605278+victor-garciacode@users.noreply.github.com>
2022-02-02 04:09:13 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,5 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("This is a treeName tree. It is a treeType and its leaves are currently leafColor. It does/does not lose its leaves for the winter.");
+  }
+}
---
+++ tree.java
@@ -0,0 +1,42 @@
+class Tree {
+String treeName;
+String treeType;
+boolean leavesFall;
+String leafColor;
+Tree(){
+  treeName = "";
+  treeType = "";
+  leavesFall = false;
+  leafColor = "";
+}
+Tree(String aTreeName, String aTreeType, boolean aLeavesFall, String aLeafColor){
+  treeName = aTreeName;
+  treeType = aTreeType;
+  leavesFall = aLeavesFall;
+  leafColor = aLeafColor;
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
